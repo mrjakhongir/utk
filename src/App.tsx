488636a4d55bfef7a391ad72b95ee732a96e1afc
Jahongir <nusratovjahongir@gmail.com>
2025-03-15 14:58:15 +0500

    feat: complete main page UI.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./app.scss";
+
+function App() {
+  return (
+    <div className='app'>
+      <button className='app__button'>Расчет платежей</button>
+    </div>
+  );
+}
+
+export default App;
